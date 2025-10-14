f705f4079cdb600139b8e41cfe9b14493a2c7e51
double07killor <75599140+double07killor@users.noreply.github.com>
2025-10-13 23:17:20 -0500

    Remove old embedded legacy folders

--- Legacy SourceCode -V6/AgOpenGPS.Core/Interfaces/Presenters/IApplicationPresenter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AgOpenGPS.Core.Interfaces
-{
-    public interface IApplicationPresenter
-    {
-        IErrorPresenter ErrorPresenter { get; }
-    }
-}
-
--- Legacy SourceCode -V6/AgOpenGPS.Core/Models/Vehicle/BrandEnums.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AgOpenGPS.Core.Models
-{
-    public enum TractorBrand { AGOpenGPS, Case, Claas, Deutz, Fendt, JohnDeere, Kubota, Massey, NewHolland, Same, Steyr, Ursus, Valtra, JCB }
-    public enum HarvesterBrand { AgOpenGPS, Case, Claas, JohnDeere, NewHolland }
-    public enum ArticulatedBrand { AgOpenGPS, Case, Challenger, JohnDeere, NewHolland, Holder }
-}

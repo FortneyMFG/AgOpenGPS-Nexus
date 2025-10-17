9bafd591b3a7e07b9d1c8af48f04b8d8f38cf7ca
double07killor <75599140+double07killor@users.noreply.github.com>
2025-10-17 18:16:07 -0500

    NX-995 Restore dispatcher helper usage

--- Nexus SourceCode/src/Aog.UI.Avalonia/ViewModels/SimulationBarViewModel.cs
+++ Nexus SourceCode/src/Aog.UI.Avalonia/ViewModels/SimulationBarViewModel.cs
@@ -494,9 +494,9 @@ private void SortPlaybackRates()
 
     private void NotifyPlaybackRateProperties()
     {
-        RaisePropertyChanged(nameof(SelectedPlaybackRate));
-        RaisePropertyChanged(nameof(SelectedPlaybackRateLabel));
-        RaisePropertyChanged(nameof(SelectedPlaybackRateOption));
+        OnPropertyChanged(nameof(SelectedPlaybackRate));
+        OnPropertyChanged(nameof(SelectedPlaybackRateLabel));
+        OnPropertyChanged(nameof(SelectedPlaybackRateOption));
     }
 
 
@@ -653,10 +653,7 @@ private void SortPlaybackRates()
 
     private void ExecuteOnDispatcher(Action action)
     {
-        if (action is null)
-        {
-            return;
-        }
+        ArgumentNullException.ThrowIfNull(action);
 
         if (_dispatcher.CheckAccess())
         {
